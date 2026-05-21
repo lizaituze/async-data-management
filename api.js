00ace533d0094cc0483fef87cc503593c454dac4
Liza Ituze <lizaituze1@gmail.com>
2026-05-21 21:54:20 +0200

    I am done with api.js

--- api.js
+++ api.js
@@ -1,4 +1,33 @@
-function fetchPosts() {
-    return fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json());
+
+export function fetchPosts(){
+
+return fetch("https://jsonplaceholder.typicode.com/posts")
+
+.then(response=>{
+
+if(!response.ok){
+
+throw new Error(
+"Failed to fetch posts"
+);
+
+}
+
+return response.json();
+
+})
+
+.then(data=>{
+
+return data;
+
+})
+.catch(error=>{
+
+console.error(error);
+
+return [];
+
+});
+
 }
